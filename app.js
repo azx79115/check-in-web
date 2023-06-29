04a6bb4230120445c1a0b98132ad589aba2e800a
azx79115 <azx79115@gmail.com>
2023-06-29 22:54:09 +0800

    完成依登入狀態切換導覽頁按鈕(未排版)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
 const router = require("./routes");
 const path = require("path");
@@ -9,6 +10,18 @@ const session = require("express-session");
 const passport = require("passport");
 const flash = require("connect-flash");
 
+// 樣板引擎
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: ".hbs",
+  handlebars: Handlebars,
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+  },
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+//body資料
 app.use(express.urlencoded({ extended: true }));
 // 載入靜態資料
 app.use(
@@ -32,11 +45,14 @@ app.use(
 );
 app.use(passport.initialize()); // 初始化Passport
 app.use(passport.session()); // 啟動session功能
+//套用快閃訊息
 app.use(flash());
-// 樣板引擎
-app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
-app.set("view engine", "hbs");
-
+//res.local
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 //路由
 app.use(router);
 
