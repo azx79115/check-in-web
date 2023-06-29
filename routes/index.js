870474b246488ba99671875b386b3ac43a10e066
azx79115 <azx79115@gmail.com>
2023-06-29 22:18:16 +0800

    驗證登入狀態

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,17 @@ const admin = require("./modules/admin");
 const home = require("./modules/home");
 const user = require("./modules/user");
 const login = require("./modules/login");
+const { authenticator } = require("../middleware/auth");
 
 //紀錄
-router.use("/record", record);
+router.use("/record", authenticator, record);
 //後台
-router.use("/admin", admin);
+router.use("/admin", authenticator, admin);
+//使用者
+router.use("users", authenticator, user);
 //登入
 router.use("/login", login);
 //首頁
-router.use("/", home);
+router.use("/", authenticator, home);
 
 module.exports = router;
