681d81386b5407b34ac7afb2a7e3dac476f58fa2
azx79115 <azx79115@gmail.com>
2023-07-05 11:36:17 +0800

    修改PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const Handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
 const router = require("./routes");
